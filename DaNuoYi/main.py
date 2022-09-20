cb2eb8e759dbb6f52c66c6ab534faa5260fc8cc8
Ehsan Ghaffari Sadr <30634010+EsiLambda@users.noreply.github.com>
2022-09-20 11:40:06 +0430

    Change evolve_round and pop_size from 20 and 100 to 5 and 20

--- DaNuoYi/main.py
+++ DaNuoYi/main.py
@@ -68,8 +68,8 @@ def quick_run(tasks=None, classifier_name='lstm', waf='mod_security', seed=None,
     arguments.add_argument("--tasks", default=tasks, type=list, help="injection tasks")
     arguments.add_argument("--waf", default=waf, choices=['mod_security', 'openresty'])
     arguments.add_argument("--classifier", default=classifier_name, choices=['lstm', 'rnn', 'gru'], help="Type of classifier to load")
-    arguments.add_argument("--evolve_round", default=20, help="Maximum number of fuzzing rounds")
-    arguments.add_argument("--pop_size", default=100, help="Fuzzing step size for each round (parallel fuzzing steps)")
+    arguments.add_argument("--evolve_round", default=5, help="Maximum number of fuzzing rounds")
+    arguments.add_argument("--pop_size", default=20, help="Fuzzing step size for each round (parallel fuzzing steps)")
     arguments = arguments.parse_args()
 
     logger = Logger(arguments.tasks, arguments.classifier)
